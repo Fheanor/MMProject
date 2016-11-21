f1fcb70f5795783906b7075643ac0d2f685dfac8
ghamm03 <hammouti.guillaume@gmail.com>
2016-11-21 12:15:41 +0100

    Ajout du menu circulaire

---
+++ app/src/main/java/com/ihm/mymuseum/menu/RadialMenuMainFragment.java
@@ -0,0 +1,22 @@
+package com.ihm.mymuseum.menu;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.ihm.mymuseum.R;
+/**
+ *
+ * @author Arindam Nath
+ *
+ */
+public class RadialMenuMainFragment extends Fragment{
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view  = inflater.inflate(R.layout.layout_main, container, false);
+		return view;
+	}
+}
